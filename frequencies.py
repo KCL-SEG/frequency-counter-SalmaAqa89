7b18bdc3745799a5aedcf0e9f64edfda40f84548
Salma Aqarrout <salma.aqarrout@kcl.ac.uk>
2023-09-30 02:34:04 +0100

    Complete frequency assignment

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,15 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    new_list= []
+    for item in items:
+        new_list.append(str(item))
+    for item in new_list:
+        if item in frequencies:
+            frequencies[item] +=1
+        else:
+            frequencies[item] = 1
     return frequencies
+
+result = frequencies([100, 'Hello', '100', '100', 100])
+print(result)
