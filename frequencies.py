4520284a0a3dd3b1556f82fe5b8213b5075198aa
Salma Aqarrout <salma.aqarrout@kcl.ac.uk>
2023-12-06 22:24:01 +0000

    Update frequencies.py

--- frequencies.py
+++ frequencies.py
@@ -3,15 +3,11 @@
 
 def frequencies(items):
     frequencies = {}
-    new_list= []
     for item in items:
-        new_list.append(str(item))
-    for item in new_list:
-        if item in frequencies:
-            frequencies[item] +=1
+        key = str(item)
+        if item not in frequencies.keys():
+            frequencies[item] =1
         else:
-            frequencies[item] = 1
+            frequencies[item] += 1
     return frequencies
 
-result = frequencies([100, 'Hello', '100', '100', 100])
-print(result)
